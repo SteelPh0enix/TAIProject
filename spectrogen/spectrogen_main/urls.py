14e7d1d453dd9f2555347f03ec06a4c0785305b2
SteelPh0enix <steelphoenix2137@gmail.com>
2020-01-04 10:57:39 +0100

    Logging in works

--- spectrogen/spectrogen_main/urls.py
+++ spectrogen/spectrogen_main/urls.py
@@ -6,4 +6,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('login/', views.login_user),
+    path('logout/', views.logout_user),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- spectrogen/spectrogen_main/views.py
+++ spectrogen/spectrogen_main/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, 'index.html')
+
+
+def login_user(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+
+    user = authenticate(request, username=username, password=password)
+    print("User: {0}".format(user))
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect('/')
+    return render(request, 'index.html', context={'login_failure': True})
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect('/')
