1f67d861d41d12cde31fbb0c25418189b6e396b0
SteelPh0enix <steelphoenix2137@gmail.com>
2020-01-09 16:52:07 +0100

    Created single function for spectrogram generation and test

--- spectrum_generator/generate.py
+++ spectrum_generator/generate.py
@@ -1,60 +1,6 @@
-from pydub import AudioSegment
-import matplotlib.pyplot as plt
-import numpy as np
-import youtube_dl
-
 import spectrogen
 
-FILENAME = 'fantano.wav'
 YT_URL = 'https://www.youtube.com/watch?v=yAF9XlluONA'
 
-def finish_hook(data):
-    global FILENAME
-    if data['status'] == 'finished':
-        print('Downloaded video {0}'.format(data['filename']))
-        FILENAME = data['filename'][:data['filename'].rindex('.')] + '.mp3'
-
-
-def download_audio(url):
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '320'
-        }],
-        'progress_hooks': [finish_hook]
-    }
-
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-
-
-def main():
-    download_audio(YT_URL)
-
-    sound = AudioSegment.from_file(FILENAME)
-    mono_sound = sound.split_to_mono()[0]
-
-    samples = np.array(mono_sound.get_array_of_samples())
-    freq = mono_sound.frame_rate
-
-    print("Frequency: {0}".format(freq))
-
-    fig, ax = plt.subplots(1)
-    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
-    ax.axis('off')
-
-    plt.specgram(samples, Fs=freq, pad_to=pow(2, 16),
-                 NFFT=pow(2, 10), noverlap=pow(2, 2))
-    plt.yscale('log')
-    plt.ylim((100, 21000))
-
-    ax.axis('off')
-    fig.set_size_inches(30, 12, forward=True)
-    fig.savefig('spectrum.png', dpi=100, facecolor=None, transparent=True)
-
-
 if __name__ == '__main__':
-    # main()
-    print('Filename: {0}'.format(spectrogen.download_audio(YT_URL)))
+    spectrogen.create_spectrogram_from_video(YT_URL, './generated')
--- spectrum_generator/spectrogen.py
+++ spectrum_generator/spectrogen.py
@@ -2,13 +2,15 @@ from pydub import AudioSegment
 import matplotlib.pyplot as plt
 import numpy as np
 import youtube_dl as ydl
+import os
 
 
 def replace_extension(filename: str, new_ext: str) -> str:
     return filename[:filename.rindex('.')] + '.' + new_ext
 
 
-def download_audio(url: str,output_dir: str) -> str:
+def download_audio(url: str, output_dir: str) -> str:
+    """Downloads audio from YouTube"""
     ydl_options = {
         'format': 'bestaudio',
         'outtmpl': output_dir + '/%(id)s.%(ext)s',
@@ -26,6 +28,7 @@ def download_audio(url: str,output_dir: str) -> str:
 
 
 def extract_raw_audio(input_filename: str, start: int = 0, end: int = -1):
+    """Extracts raw audio from MP3 file"""
     audio = AudioSegment.from_mp3(input_filename)
     first_channel = audio.split_to_mono()[0]
 
@@ -43,6 +46,7 @@ def extract_raw_audio(input_filename: str, start: int = 0, end: int = -1):
 
 
 def generate_spectrogram(data, frequency: int, output_filename: str) -> None:
+    """Generated spectrogram from raw data"""
     figure, axis = plt.subplots(1)
 
     figure.subplots_adjust(left=0, right=1, bottom=0, top=1)
@@ -54,5 +58,24 @@ def generate_spectrogram(data, frequency: int, output_filename: str) -> None:
     plt.ylim((100, 20000))
 
     axis.axis('off')
-    figure.set_size_inches(10, 8, forward=True)
+    figure.set_size_inches(15, 8, forward=True)
     figure.savefig(output_filename, dpi=100, facecolor=None, transparent=True)
+
+
+def create_spectrogram_from_video(url: str, output_dir: str, start: int = 0, end: int = -1,
+                                  temp_cache_dir: str = './tempcache') -> str:
+    """Creates spectrogram from YouTube video and returns it's filename"""
+    audio_file = download_audio(url, temp_cache_dir)
+
+    raw_audio, audio_frequency = extract_raw_audio(audio_file, start, end)
+
+    spectrogram_file = output_dir + '/' + os.path.basename(replace_extension(audio_file, 'png'))
+
+    if not os.path.exists(os.path.dirname(spectrogram_file)):
+        os.mkdir(os.path.dirname(spectrogram_file))
+
+    generate_spectrogram(raw_audio, audio_frequency, spectrogram_file)
+
+    os.remove(audio_file)
+
+    return spectrogram_file
